a885e5fb4f7d0f0935bd92deb6db4d1c38eb6dbd
Valdis2102 <102295101+Valdis2102@users.noreply.github.com>
2022-12-24 17:33:48 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,27 @@
+    let rand = document.getElementById('button');
+    rand.onclick = function go(){
+        let start_num = document.getElementById('start_num').value;    
+        let finish_num = document.getElementById('finish_num').value;
+        let num = Math.random()*finish_num;
+        num = num.toFixed(0);
+        if(num < start_num){ return go()};
+        rand.value = num;
+        console.log(rand.value);
+        document.getElementById('effect').innerHTML = 'Guess number';
+        document.getElementById('choice').value = '';
+        return rand.value;
+    };
+
+    let button = document.getElementById('button_inp');
+    button.onclick = function check(){
+        let choice = document.getElementById('choice').value;
+        if (choice > rand.value){
+            document.getElementById('effect').innerHTML = 'Less'
+        } if (choice < rand.value){
+            document.getElementById('effect').innerHTML = 'More'
+        } if (choice == rand.value){
+            document.getElementById('effect').innerHTML = 'You guessed!'
+        }
+    }
+
+    
